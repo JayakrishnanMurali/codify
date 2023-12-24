034d25f395fb1ed0534cd99a1ef81f29b9fbcb89
Jayakrishnan M <jayakrishnan1012@gmail.com>
2023-12-24 13:07:23 +0530

    Refactor Clarity Analytics code

--- src/analytics/clarity-analytics.tsx
+++ src/analytics/clarity-analytics.tsx
@@ -3,12 +3,18 @@ import Script from "next/script";
 import React from "react";
 
 export const ClarityAnalytics = () => {
-  const clarityCode = env.NEXT_PUBLIC_CLARITY_ANALYTICS;
+  const code = env.NEXT_PUBLIC_CLARITY_ANALYTICS;
+
+  const clarityCode = `
+(function (c,l,a,r,i,t,y){
+    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window, document, "clarity", "script", "${code}");`;
+
   return (
-    <Script
-      type="text/javascript"
-      strategy="afterInteractive"
-      src={`https://www.clarity.ms/tag/${clarityCode}`}
-    />
+    <Script id="ms-clarity" strategy="afterInteractive">
+      {clarityCode}
+    </Script>
   );
 };
